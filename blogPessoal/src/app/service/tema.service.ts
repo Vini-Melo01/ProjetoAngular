7c90504585d05202a5d9f05aa81fa4cd8d99c88c
Konaaz <vinicius.melo.rodrigues@hotmail.com>
2021-05-06 16:47:54 -0300

    put e delete dos temas

--- blogPessoal/src/app/service/tema.service.ts
+++ blogPessoal/src/app/service/tema.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { observable, Observable } from 'rxjs';
 import { environment } from '../../environments/environment.prod';
 import { Tema } from '../model/Tema';
 
@@ -20,8 +20,21 @@ export class TemaService {
 
   }
 
+  getByIdTema(id:number):Observable<Tema>{
+    return this.http.get<Tema>(`http://localhost:8080/tema/${id}`,this.token)
+
+  }
+
   postTema(tema:Tema):Observable<Tema>{
     return this.http.post<Tema>('http://localhost:8080/tema',tema, this.token)
 
+  }
+
+  putTema(tema:Tema):Observable<Tema>{
+    return this.http.put<Tema>('http://localhost:8080/tema', tema,this.token)
+  }
+
+  deleteTema(id:number){
+    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token) //Abrir crase ao inves de aspas pq ta pedindo um parametro de id la no back para fazer o delete
   }
 }
