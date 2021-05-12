a82d7c2387c58aa8e27bdf20229f4abd2c15a5ab
Konaaz <vinicius.melo.rodrigues@hotmail.com>
2021-05-12 14:01:11 -0300

    put-delete-postagem

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,7 +1,7 @@
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment.prod';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.prod';
 import { Postagem } from '../model/Postagem';
 
 @Injectable({
@@ -17,21 +17,22 @@ export class PostagemService {
 
   getAllPostagens(): Observable<Postagem[]>{
     return this.http.get<Postagem[]>('http://localhost:8080/postagens', this.token)
-
   }
-  getByIdPostagem(id:number): Observable<Postagem>{
+
+  getByIdPostagem(id: number): Observable<Postagem>{
     return this.http.get<Postagem>(`http://localhost:8080/postagens/${id}`, this.token)
   }
 
-  postPostagem(postagem: Postagem): Observable<Postagem>{
+  postPostagem(postagem: Postagem) : Observable<Postagem>{
     return this.http.post<Postagem>('http://localhost:8080/postagens', postagem, this.token)
-
   }
 
   putPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.put<Postagem>('http://localhost:8080/postagens', postagem, this.token)
   }
-  deletePostagem(id:number){
-    return this.http.delete(`http://localhost:8080/postagens/${id}`,this.token)
+
+  deletePostagem(id: number){
+    return this.http.delete(`http://localhost:8080/postagens/${id}`, this.token)
   }
+
 }
